d3b3e8a3734de5b6eae58f19f9d47448f7e7f0ca
InnocentWahome <innocentwahome@gmail.com>
2024-01-01 21:21:11 +0300

    new home route

--- start/routes.ts
+++ start/routes.ts
@@ -1,25 +1,13 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+
+Route.get('/health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+
+  return report.healthy ? response.ok(report) : response.badRequest(report)
+})
+
 Route.get('/', async () => {
-  return { hello: 'world' }
+  return 'App running 🚀'
 })
