126c5c4c38fb0a6082b51a2e179eedfc0f432563
InnocentWahome <innocentwahome@gmail.com>
2024-01-06 00:43:50 +0300

    add custom records on db seed

--- database/seeders/1000_users.ts
+++ database/seeders/1000_users.ts
@@ -1,8 +1,44 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
 import { UserFactory } from 'Database/factories/UserFactory'
+import User from 'App/Models/User'
 
 export default class UserSeeder extends BaseSeeder {
   public async run() {
+    // Create 10 generic user records using the factory
     await UserFactory.createMany(10)
+
+    // Create specific user records
+    await User.createMany([
+      {
+        full_name: 'Wahome Farmer',
+        username: 'wahome',
+        email: 'farmer@farmer.com',
+        password: 'password',
+        phone_number: '254712345678',
+        user_type: 'farmer',
+        profile_url: 'https://i.pravatar.cc/300',
+        is_verified: true,
+      },
+      {
+        full_name: 'Maina User',
+        username: 'user',
+        email: 'user@user.com',
+        password: 'password',
+        phone_number: '2543232712345678',
+        user_type: 'customer',
+        profile_url: 'https://i.pravatar.cc/300',
+        is_verified: true,
+      },
+      {
+        full_name: 'Innocent SuperAdmin',
+        username: 'i_am_admin',
+        email: 'admin@admin.com',
+        password: 'password',
+        phone_number: '25443434712345678',
+        user_type: 'super_admin',
+        profile_url: 'https://i.pravatar.cc/300',
+        is_verified: true,
+      },
+    ])
   }
 }
