8d6577d9b3da7781fe2a7be151e5fa41f1cdba0d
Innocent Maina <62073759+innocent-maina@users.noreply.github.com>
2024-03-25 16:52:29 +0300

    delete delivery module

--- database/seeders/2000_deliveries.ts
+++
@@ -1,8 +0,0 @@
-import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
-import { DeliveriesFactory } from 'Database/factories/DeliveriesFactory'
-
-export default class DeliveriesSeeder extends BaseSeeder {
-  public async run() {
-    await DeliveriesFactory.with('user', 5).createMany(10)
-  }
-}
